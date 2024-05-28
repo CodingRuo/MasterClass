8130484e67806865da0e8111c274ef71bf9b0c91
CodingRuo <r.schober@outlook.com>
2024-05-28 17:35:36 +0200

    Section 2 Done

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang=ts>
+
+</script>
+
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <RouterLink to="/projects">Got to Projects</RouterLink>
+    </div>
+</template>
+
+<style scoped></style>
